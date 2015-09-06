c0d15b657949a231b7743bda006c67f3670c3137
James Yanyuk <jyanyuk@gmail.com>
2015-09-06 17:34:25 -0400

    Implementing networked messaging system

--- core/src/com/insight/networking/NetworkingStore.java
+++ core/src/com/insight/networking/NetworkingStore.java
@@ -4,5 +4,20 @@ package com.insight.networking;
  * Created by jamesyanyuk on 9/6/15.
  */
 public class NetworkingStore {
+  private NetworkingToken token;
 
+  public NetworkingStore() {}
+
+  public NetworkingStore(NetworkingToken token) {
+    setToken(token);
+  }
+
+  public NetworkingToken getToken() {
+    return token;
+  }
+
+  public NetworkingToken setToken(NetworkingToken token) {
+    this.token = token;
+    return token;
+  }
 }
--- core/src/com/insight/networking/NetworkingToken.java
+++ core/src/com/insight/networking/NetworkingToken.java
@@ -4,6 +4,20 @@ package com.insight.networking;
  * Created by jamesyanyuk on 9/6/15.
  */
 public class NetworkingToken {
-  public String tokenVal;
+  private String tokenVal;
 
+  public NetworkingToken() {}
+
+  public NetworkingToken(String tokenVal) {
+    setValue(tokenVal);
+  }
+
+  public String getValue() {
+    return tokenVal;
+  }
+
+  public String setValue(String tokenVel) {
+    this.tokenVal = tokenVal;
+    return tokenVal;
+  }
 }
